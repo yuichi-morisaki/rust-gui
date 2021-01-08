da187b189dccef0b86534eb00691c0f698435478
yuichi-morisaki <yuichi.morisaki@gmail.com>
2021-01-08 18:47:47 +0900

    Refactor to enable undoable flip in try_move of Board

--- gtk-examples/othello/src/board.rs
+++ gtk-examples/othello/src/board.rs
@@ -1,221 +1,141 @@
-use crate::position::Position;
+use crate::position::Coordinate;
+use std::collections::HashMap;
 
-use std::fmt;
-
-#[repr(u8)]
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Disk {
-    Black = 0,
-    White = 1,
+    Black,
+    White,
 }
 
-impl PartialEq for Disk {
-    fn eq(&self, other: &Self) -> bool {
-        *self as u8 == *other as u8
+fn flip_disk(disk: &Disk) -> Disk {
+    match disk {
+        Disk::Black => Disk::White,
+        Disk::White => Disk::Black,
     }
 }
 
-// --------------------------------------------------------------------
+// ---------------------------------------------------------------------
 
-#[repr(u8)]
-#[derive(Clone, Copy)]
-pub enum Side {
-    Dark = 0,
-    Light = 1,
+pub enum MoveResult {
+    NotEmpty,
+    NoDiskFlipped,
 }
 
-pub fn change_turn(current: Side) -> Side {
-    match current {
-        Side::Dark => Side::Light,
-        Side::Light => Side::Dark,
-    }
-}
+// ---------------------------------------------------------------------
 
-// --------------------------------------------------------------------
-
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct Board {
-    disks: [Option<Disk>; 100],
+    disks: HashMap<Coordinate, Disk>,
+    stack: Vec<Coordinate>,
 }
 
 impl Board {
     pub fn new() -> Board {
-        let mut disks = [None; 100];
-
-        disks[Position::from(('d', 4)).index()] = Some(Disk::White);
-        disks[Position::from(('d', 5)).index()] = Some(Disk::Black);
-        disks[Position::from(('e', 4)).index()] = Some(Disk::Black);
-        disks[Position::from(('e', 5)).index()] = Some(Disk::White);
-
-        Board { disks }
+        Board {
+            disks: HashMap::with_capacity(64),
+            stack: Vec::with_capacity(18),
+        }
     }
 
-    pub fn set_disk(&mut self, pos: Position, disk: Disk) {
-        self.disks[pos.index()] = Some(disk);
+    pub fn init(&mut self) {
+        self.disks.clear();
+        self.stack.clear();
+
+        self.place(Coordinate::new('d', 5), Disk::Black);
+        self.place(Coordinate::new('e', 4), Disk::Black);
+        self.place(Coordinate::new('d', 4), Disk::White);
+        self.place(Coordinate::new('e', 5), Disk::White);
     }
 
-    pub fn get_disk(&self, pos: Position) -> Option<Disk> {
-        self.disks[pos.index()]
+    pub fn get_disk(&self, coord: Coordinate) -> Option<Disk> {
+        match self.disks.get(&coord) {
+            None => None,
+            Some(&disk) => Some(disk),
+        }
     }
 
-    pub fn try_place(&self, pos: Position, side: Side) -> Option<Board> {
-        if self.get_disk(pos).is_some() {
-            return None;
+    pub fn try_move(
+        &self,
+        coord: Coordinate,
+        disk: Disk,
+    ) -> Result<Board, MoveResult> {
+        if self.get_disk(coord).is_some() {
+            return Err(MoveResult::NotEmpty);
         }
 
-        let (current, opponent) = match side {
-            Side::Dark => (Disk::Black, Disk::White),
-            Side::Light => (Disk::White, Disk::Black),
+        let mut board = self.clone();
+        let mut num_flip = 0;
+        let (current, opponent) = match disk {
+            Disk::Black => (Disk::Black, Disk::White),
+            Disk::White => (Disk::White, Disk::Black),
         };
-        let mut positions = vec![];
-        for &dx in &[-1, 0, 1] {
-            for &dy in &[-1, 0, 1] {
+
+        for dx in -1..=1 {
+            for dy in -1..=1 {
                 if dx == 0 && dy == 0 {
                     continue;
                 }
-                for f_pos in self.flip_positions(
-                    pos.col_and_row(),
-                    (dx, dy),
-                    (current, opponent),
-                ) {
-                    positions.push(f_pos);
+                for offset in 1.. {
+                    if let Ok(coord) = coord + (dx, dy) {
+                        if let Some(disk) = board.get_disk(coord) {
+                            if disk == current {
+                                num_flip += board.commit();
+                                break;
+                            }
+                            if disk == opponent {
+                                board.flip(coord);
+                                continue;
+                            }
+                        }
+                    }
+                    board.abort();
+                    break;
                 }
             }
         }
-        if positions.len() == 0 {
-            return None;
-        }
 
-        let mut board = *self;
-        board.set_disk(pos, current);
-        for pos in positions {
-            board.set_disk(pos, current);
-        }
-
-        Some(board)
-    }
-
-    fn flip_positions(
-        &self,
-        (mut col, mut row): (i8, i8),
-        (dx, dy): (i8, i8),
-        (current, opponent): (Disk, Disk),
-    ) -> Vec<Position> {
-        let mut positions = vec![];
-        loop {
-            col += dx;
-            row += dy;
-            let pos = Position::from((col, row));
-            if let Some(disk) = self.get_disk(pos) {
-                if disk == current {
-                    return positions;
-                }
-                if disk == opponent {
-                    positions.push(pos);
-                }
-            } else {
-                return vec![];
-            }
+        if num_flip > 0 {
+            Ok(board)
+        } else {
+            Err(MoveResult::NoDiskFlipped)
         }
     }
-}
 
-impl fmt::Display for Board {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "    a   b   c   d   e   f   g   h\n")?;
-        for index in 0..100 {
-            let row = index / 10;
-            let col = index % 10;
-            if row == 0 || row == 9 || col == 0 || col == 9 {
-                continue;
-            }
-            if col == 1 {
-                write!(f, "  +---+---+---+---+---+---+---+---+\n{} |", row)?;
-            }
-            let disk = match self.disks[index] {
-                None => ' ',
-                Some(Disk::Black) => 'x',
-                Some(Disk::White) => 'o',
-            };
-            write!(f, " {} |", disk)?;
-
-            if col == 8 {
-                write!(f, "\n")?;
-            }
+    fn abort(&mut self) {
+        if let Some(_) = self.undo_flip() {
+            self.abort();
         }
-        write!(f, "  +---+---+---+---+---+---+---+---+\n")
-    }
-}
-
-// ====================================================================
-
-#[cfg(test)]
-mod tests {
-    use crate::position::Position;
-
-    use super::Side;
-
-    use super::Board;
-
-    #[test]
-    fn board_new() {
-        let board = Board::new();
-        let output = "    \
-    a   b   c   d   e   f   g   h
-  +---+---+---+---+---+---+---+---+
-1 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-2 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-3 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-4 |   |   |   | o | x |   |   |   |
-  +---+---+---+---+---+---+---+---+
-5 |   |   |   | x | o |   |   |   |
-  +---+---+---+---+---+---+---+---+
-6 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-7 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-8 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-";
-        assert_eq!(board.to_string(), output);
     }
 
-    #[test]
-    fn board_try_place_some() {
-        let board = Board::new();
-        let pos = Position::from(('c', 4));
-        let board = board.try_place(pos, Side::Dark).unwrap();
-        let output = "    \
-    a   b   c   d   e   f   g   h
-  +---+---+---+---+---+---+---+---+
-1 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-2 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-3 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-4 |   |   | x | x | x |   |   |   |
-  +---+---+---+---+---+---+---+---+
-5 |   |   |   | x | o |   |   |   |
-  +---+---+---+---+---+---+---+---+
-6 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-7 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-8 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-";
-        assert_eq!(board.to_string(), output);
+    fn commit(&mut self) -> usize {
+        let size_of_stack = self.stack.len();
+        self.stack.clear();
+
+        size_of_stack
     }
 
-    #[test]
-    fn board_try_place_none() {
-        let board = Board::new();
-        let pos = Position::from(('c', 4));
-        let board = board.try_place(pos, Side::Light);
-        assert!(board.is_none());
+    fn place(&mut self, coord: Coordinate, disk: Disk) {
+        if let Some(_) = self.disks.insert(coord, disk) {
+            panic!("can't place - not empty");
+        }
+    }
+
+    fn flip(&mut self, coord: Coordinate) {
+        if let Some(disk) = self.disks.get_mut(&coord) {
+            *disk = flip_disk(disk);
+            self.stack.push(coord);
+        } else {
+            panic!("can't flip - no disk");
+        }
+    }
+
+    fn undo_flip(&mut self) -> Option<Coordinate> {
+        if let Some(coord) = self.stack.pop() {
+            let disk = self.disks.get_mut(&coord).unwrap();
+            *disk = flip_disk(disk);
+            Some(coord)
+        } else {
+            None
+        }
     }
 }
